6de9bfe0b7ed46ed7138cf66dd536fa2e45e5960
Julian Durnwalder <durnwalder@rocketmail.com>
2016-10-10 12:03:26 +0200

    new Function CeToFa

--- p1.py
+++ p1.py
@@ -1,14 +1,19 @@
 def FaToCe (fahrin):
 	celsiusout = 5./9 * (fahrin - 32) # conversion
 	return(celsiusout)
+def CeToFa (celsin):
+	fahrout = celsin*9./5+32
+	return(fahrout)
 lower = 0
 upper = 300
 step = 20
-
+cels = lower
 fahr = lower
 while ( fahr < upper ): 
     
-    print "Fahrenheit ", fahr," = Celsius ", FaToCe(fahr)
-    fahr += step  # increment
+	print "Fahrenheit ", fahr," = Celsius ", FaToCe(fahr)
+	print "Celsius ", cels," = Fahrenheit ", CeToFa(cels)
+	cels += step  # increment
+	fahr += step
 
 # end of loop
